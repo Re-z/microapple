e2ffeeb03ae089368ae3dcf5d2e304c92d612a0f
anton <re-z@ukr.net>
2016-07-13 17:18:09 +0300

    almost done

--- build/js/common.js
+++ build/js/common.js
@@ -1,28 +1,31 @@
 $(document).ready(function() {
-    //toggling meny
+
+    //toggling menu
 	$('.ms__nav-item').on('click', function () {
         $('.ms__nav-item').removeClass('is-active');
         $(this).addClass('is-active');
-        $('.ms__nested').slideUp();
-        $(this).find('.ms__nested').slideToggle();
-        $('.ms__nav-item.is-active').click(function(){
-
-        });
-        //
-        //var nestedLists = $('.ms__nested');
-        //var thisNestedList = $(this).find('.ms__nested');
-        //
-        //$(nestedLists).slideUp();
-        //$(this).toggleClass('is-active');
-        //$(thisNestedList).slideToggle();
+        $(this).find('.ms__nested').slideToggle().end().siblings().find('.ms__nested').slideUp();
+        event.stopPropagation()
     });
 
+    //hide menu on body-click
+    $('body').on('click', function(){
+        $('.ms__nav-item').removeClass('is-active');
+        $('.ms__nested').slideUp();
+    });
 
+    //show-hide subnested lists
     $('.ms__nested-item').hover(function(){
         var subNestedList = $(this).find('.ms__subnested');
         $(subNestedList).toggle()
+    });
 
-    })
+    //toggling placeholder attribute
+    $('.ms__search-query').on('focus',function(){
+        $(this).removeAttr('placeholder')
+    }).on('focusout', function () {
+        $(this).attr('placeholder', 'Search Microsoft.com')
+    });
 
 
 });
